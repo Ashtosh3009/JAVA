b4f6879503a74cb3031f33ab580feddbb7bf2b2c
Aditya Kumar <adityakumar@Adityas-MacBook-Pro.local>
2021-06-25 23:34:28 +0530

    adding

---
+++ jp25juneq4.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class jp25juneq4 {
+    public static void main(String[] args)
+    {
+        Scanner obj =new Scanner (System.in);
+        System.out.println("Enter the quantity of object u want  ");
+        int a =obj.nextInt();
+        System.out.println("Total cost is "+a*100);
+
+    }
+}
