be64fc2e29b12c4d55b286f888565b6f1dc22b9b
Aditya Kumar <71920316+Adityakr123@users.noreply.github.com>
2021-06-25 20:31:36 +0530

    Adding java programs

---
+++ array.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public  class array{
+   
+    public static void main(String[] args)
+    {
+        Scanner obj =new Scanner (System.in);
+        int n;
+        int a[]=new int[100];
+        n=obj.nextInt(); 
+        for(int i=0;i<n;i++)
+        {
+            a[i]=obj.nextInt();
+        }
+        int i=0;
+        while(i<n)
+        {
+            System.out.print(a[i]);
+            i++;
+        }
+
+       
+    }
+
+}
---
+++ firstprogram.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public  class firstprogram {
+   
+    public static void main(String[] args)
+    {
+        Scanner obj =new Scanner (System.in);
+        String s=obj.next();
+        int a =obj.nextInt();
+        int b=obj.nextInt(); 
+        System.out.println("\n");
+        System.out.println(a+" "+b+" "+s);
+        System.out.println(s);
+        
+        adi();
+       
+    }
+    public static void adi(){
+        System.out.println("adi");
+    }
+}
